26485e6ea3040db04db3a7857b18d0b69e591f44
Sailesh Kumar <saileshkl63072@gmail.com>
2026-07-23 18:25:49 +0530

    Time: 4 ms (99.33%), Space: 47.8 MB (27.89%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -3,7 +3,6 @@ class Solution {
         while(i<j){
             if(s.charAt(i)!= s.charAt(j)){
                 return false;
-
             }
             i = i + 1;
             j = j - 1;
@@ -12,26 +11,22 @@ class Solution {
     }
    
     public boolean validPalindrome(String s) {
-       int i = 0;
-       int j = s.length()-1;
+        int i = 0;
+        int j = s.length()-1;
 
-       while(i<j){
+        while(i<j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
 
-        char left = s.charAt(i);
-        char right = s.charAt(j);
-        
-        if(left!= right){
-            return palindromeHelper(i+1 , j, s) || palindromeHelper(i, j-1, s);
+            if(left != right){
+                return palindromeHelper(i+1, j , s) || palindromeHelper(i, j-1, s);
+            }else{
+                i = i + 1;
+                j = j - 1;
 
-        }else{
-            i = i + 1;
-            j = j - 1;
-
+            }
         }
-
-
-       }
-       return true;
+      return true;
     }
        
 }
